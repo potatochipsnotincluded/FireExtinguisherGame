51a8cce4924622f1fb65ae8b5577d45e6889ed4d
Kai Tabuchi <kaidanieltabuchi@gmail.com>
2025-12-16 20:29:04 -0600

    why doesn't it work

---
+++ App/Source/Main.cpp
@@ -0,0 +1,33 @@
+#include "raylib.h"
+
+int main()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - input keys");
+
+    Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
+
+    while (!WindowShouldClose())
+    {
+        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+
+        DrawCircleV(ballPosition, 50, MAROON);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
